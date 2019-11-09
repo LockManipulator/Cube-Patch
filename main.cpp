5620f2e8a1e4616955896c3fd963e48878568dba
LockManipulator <LockManipulator@gmail.com>
2019-11-09 07:29:25 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -11,16 +11,7 @@
 
 #include "src/memory/memory_helper.h"
 
-void* Offset(void* x1, uint64_t x2) {
-	return (void*)((char*)x1 + x2);
-}
-
-void Popup(const char* title, const char* msg) {
-	MessageBoxA(0, msg, title, MB_OK | MB_ICONINFORMATION);
-}
-
 class Mod : GenericMod {
-	bool init = false;
 
 	void VanillaGliderFix() {
 		int glider_patch[] = { 0x49, 0x8B, 0xCD, 0xE8, 0x5A, 0x25, 0xDA, 0xFF, 0xF3, 0x0F, 0x10, 0xC8, 0x41, 0x0F, 0x28, 0xC7 };
@@ -31,8 +22,7 @@ class Mod : GenericMod {
 		}
 	}
 
-	void VanillaRideFix()
-	{
+	void VanillaRideFix() {
 		int ride_patch1[] = { 0x49, 0x8B, 0xCD, 0xE8, 0xDA, 0x94, 0xDA, 0xFF, 0x0F, 0x28, 0xF8 };
 		int ride_patch2[] = { 0x0F, 0x1F, 0x40, 0x00, 0xF3, 0x44, 0x0F, 0x59, 0xCF };
 		int ride_patch3[] = { 0xF3, 0x0F, 0x5C, 0xC1, 0xF3, 0x0F, 0x59, 0x05, 0xA3, 0xD5, 0x3C, 0x00, 0xF3, 0x0F, 0x58, 0x05, 0x17, 0x92, 0x3C, 0x00, 0xF3, 0x0F, 0x59, 0x05, 0x01, 0x00, 0x00, 0x00, 0xC3, 0x00, 0x00, 0xA0, 0x3F };
@@ -58,35 +48,8 @@ class Mod : GenericMod {
 		VanillaGliderFix();
 		VanillaRideFix();
 	}
-
-	virtual void OnGameTick(cube::Game* game) override {
-		if (!init) {
-			init = true;
-
-		}
-	}
-
 };
 
 EXPORT Mod* MakeMod() {
 	return new Mod();
 }
-
-/*
-To do:
-
-1. Function to get all the artifacts in the inventory for each boost
-2. Change name of each boost
-3. Modify each base stat based off number of artifacts of that type in inventory
-4. 
-
-Code caves used:
-50301
-1694B0
-1694C2
-50CB1
-5F01F
-
-Next hook: 5B63FE
-
-*/
